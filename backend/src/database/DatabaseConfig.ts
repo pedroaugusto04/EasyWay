45df961d27aaa9b9532470cc56ac0b2c3db70fc0
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-12-27 10:38:23 -0300

    fix: correcao config .env

--- backend/src/database/DatabaseConfig.ts
+++ backend/src/database/DatabaseConfig.ts
@@ -1,12 +1,25 @@
-import {Pool } from 'pg';
+import { Pool } from 'pg';
 
-export const pool = new Pool({
-  user: process.env.PG_USER,        
-  host: process.env.PG_HOST,            
-  database: process.env.PG_DATABASE,    
-  password: process.env.PG_PASSWORD,           
-  port: parseInt(process.env.PG_PORT || '5432'), 
-  ssl: {
-    rejectUnauthorized: false,
+const environment: 'dev' | 'prod' = (process.env.NODE_ENV || 'dev') as 'dev' | 'prod'; 
+
+const config = {
+  dev: {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: parseInt(process.env.PG_PORT || '5432'),
   },
-});
+  prod: {
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: parseInt(process.env.PG_PORT || '5432'),
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+};
+
+export const pool = new Pool(config[environment]);
