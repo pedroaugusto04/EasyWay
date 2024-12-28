0d9b7d4f803fa51c2585a51b1d0f87f13dcdea1c
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-12-28 01:06:20 -0300

    fix: correcao package.json

--- backend/src/config/envConfig.ts
+++ backend/src/config/envConfig.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
 
-const environment = process.env.NODE_ENV || 'dev';
+const environment = process.env.NODE_ENV || 'development';
 
 dotenv.config({ path: `.env.${environment}` });
--- backend/src/database/DatabaseConfig.ts
+++ backend/src/database/DatabaseConfig.ts
@@ -1,18 +1,18 @@
 import fs from 'fs'; 
 import { Pool } from 'pg';
 
-const environment: 'dev' | 'prod' = (process.env.NODE_ENV || 'dev') as 'dev' | 'prod'; 
+const environment: 'development' | 'production' = (process.env.NODE_ENV || 'development') as 'development' | 'production'; 
 
 
 const config = {
-  dev: {
+  development: {
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
     port: parseInt(process.env.PG_PORT || '5432'),
   },
-  prod: {
+  production: {
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
